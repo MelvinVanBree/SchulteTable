dfb0edc72cbd7da79744124ce405b6cf8e267995
Melvin van Bree <17225342+MelvinVanBree@users.noreply.github.com>
2020-02-18 22:22:34 +0100

    Exported a lot of logic out of the core into Cell and Grid

--- src/game/core.ts
+++ src/game/core.ts
@@ -1,76 +1,23 @@
 import * as PIXI from 'pixi.js';
 import { Config } from 'Game/config';
-import { Button, Grid } from "Game/components";
+import { Grid } from "Game/components";
+import { resHelper } from "Game/helpers";
 
 export class core {
     private renderer: PIXI.Application;
-    private container: PIXI.Container;
-    private grid: Grid;
-    private vh: number;
-    private vw: number;
-
+    private gridContainer: Grid;
+    //private grid: Grid;
     constructor(private viewElement: HTMLElement){
+        // Set rendering precision
         PIXI.settings.PRECISION_FRAGMENT = PIXI.PRECISION.HIGH;
-
-        this.renderer = new PIXI.Application({ width: window.innerWidth, height: innerHeight, resizeTo: viewElement , backgroundColor: Config.style.backgroundColor });
-        this.container = new PIXI.Container();
-        this.grid = new Grid();
+        this.renderer = new PIXI.Application({ width: resHelper().x, height: resHelper().y, backgroundColor: Config.style.backgroundColor });
+        this.gridContainer = new Grid();
 
         viewElement.appendChild( this.renderer.view );
-        this.renderer.stage.addChild(this.container);
-
-        this.vh = this.renderer.view.height;
-        this.vw = this.renderer.view.width;
+        this.renderer.stage.addChild(this.gridContainer);
     }
 
-    public init() {
-        this.populateGrid();
-        this.drawGrid();
+    public start() {
+        this.gridContainer.init();
     }
-
-    private randomArr():number[]{
-        //The amount of random numbers required (x.y)
-        let n = Config.settings.gridSize.x * Config.settings.gridSize.y;
-
-
-        let randomNums = Array.from({length: n}, (v, k) => k+1);
-
-        for (let i = randomNums.length - 1; i > 0; i--) {
-            const j = Math.floor(Math.random() * (i + 1));
-            [randomNums[i], randomNums[j]] = [randomNums[j], randomNums[i]];
-        }
-        return randomNums;
-    }
-
-    private populateGrid() {
-        let random = this.randomArr();
-        // Classic Row(x) Column(y) loop
-        for(let x=0; x < this.grid.getX(); x++){
-            for(let y=0; y < this.grid.getY(); y++){
-                // Add buttons to this.grid
-                this.grid.addElement(
-                    new Button(
-                        //Get random number
-                        random.pop()!,
-                        // Classic 2D grid logic:
-                        // X, Y, Weight, Width
-                        x * (this.vw / this.grid.getX()),
-                        y * this.vh / this.grid.getY(),
-                        this.vh / this.grid.getY(),
-                        this.vw / this.grid.getX()
-                    )
-                )
-            }
-        }
-
-
-    }
-
-    private drawGrid() {
-        this.grid.get().forEach(button => {
-            this.container.addChild(button.ref);
-        });
-    }
-
-
 }
