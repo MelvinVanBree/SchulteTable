d41113c8bfb3e6b8a87a878a2f43418d2784bad1
Melvin van Bree <17225342+MelvinVanBree@users.noreply.github.com>
2020-02-18 02:16:56 +0100

    Added Grid class

---
+++ src/game/components/grid.ts
@@ -0,0 +1,38 @@
+import { Button } from 'Game/components';
+import { Config } from "Game/config";
+
+export class Grid{
+    private x: number;
+    private y: number;
+    private matrix: any[][] = [];
+
+    constructor(){
+        this.x = Config.settings.gridSize.x;
+        this.y = Config.settings.gridSize.y;
+
+        /* initialize 2D array */
+        for(var i=0; i<this.x; i++) {
+            this.matrix[i] = [];
+            for(var j=0; j<this.y; j++) {
+                this.matrix[i][j] = '';
+            }
+        }
+    }
+
+
+    setElement(_x: number, _y: number, _button: Button){
+        this.matrix[_x][_y] = _button;
+    }
+
+    public logGrid(){
+        console.log(this.matrix);
+    }
+
+    getX():number{
+        return this.x;
+    }
+    getY():number{
+        return this.y;
+    }
+    
+}
